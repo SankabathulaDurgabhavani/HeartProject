68f85e2f2b3e5deba7536ef0780cc931e2cb20c4
SankabathulaDurgabhavani <119723748+SankabathulaDurgabhavani@users.noreply.github.com>
2025-06-28 12:33:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1:27017/Health");
+// mongoose.connect("mongodb://127.0.0.1:27017/Health");
+
+mongoose.connect(process.env.MONGO_URI)
 
 // User schema
 const userSchema = new mongoose.Schema({
